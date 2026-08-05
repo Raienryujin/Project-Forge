e137772b0e4827cd9457ddc2eb80a3c800cb163e
Nishan <nishaan.rowjee@gmail.com>
2026-08-05 13:28:17 +0200

    chore(eslint): ignore CommonJS build configs to prevent module is not defined errors

--- packages/config/eslint-preset.mjs
+++ packages/config/eslint-preset.mjs
@@ -3,6 +3,14 @@ import prettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
 export default [
+  {
+    ignores: [
+      "**/dist/**",
+      "**/.next/**",
+      "**/node_modules/**",
+      "**/*.config.js"
+    ],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
